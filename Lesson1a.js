27b2ff9af57531577712eaf9f5d9e219502eb0c1
Kimani-Kaminju <lansonkimani@gmail.com>
2026-07-02 13:56:43 +0300

    Added

---
+++ Lesson1a.js
@@ -0,0 +1,19 @@
+// this is a comment
+// a variable is container used to store data values
+//for use to be able to declare js we use initializers:let,const,var
+
+//let allows one to declare values that can be changed throughout the program
+let weight=56.64
+console.log(weight)
+
+weight = 57.12
+console.log(weight)
+
+//the initializer const allows us to declare variables that cant change
+const pi=3.142
+
+
+////////////////////////////////////////
+let height=20,temp =25,points =5
+
+console.log(height,temp,points)
---
+++ Lesson1b.js
@@ -0,0 +1,26 @@
+//Javascript data types
+//Javascript string : series of characters inside of quotes
+let name = "Joseph Kamau"
+console.log("The name of the person is:",name)
+console.log(typeof(name))
+
+//Js float
+let myNumber = ("The quantity")
+
+
+//Js Boolean:This returns to true or false
+let isAdult = true
+let isRegistered = true
+
+console.log("Is Joseph an Adult",isAdult)
+console.log("Is Joseph registere",isRegistered)
+console.log(typeof(isAdult))
+
+//undefined:declared but there is no value that has been assigned to it
+let county;
+console.log("Which county are you from",county)
+console.log(typeof(county))
+
+//js null
+let username = null
+console.log("What is your username?",username)
