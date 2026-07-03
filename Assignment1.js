82b0389ea182ad81a04937bc6cf9622d5dcdeb3f
Kimani-Kaminju <lansonkimani@gmail.com>
2026-07-03 12:04:33 +0300

    Added

---
+++ Assignment1.js
@@ -0,0 +1,11 @@
+const username = "Kimani";
+const password = "12345";
+const email = "kimani@gmail.com";
+
+console.log("Username:", username);
+console.log("Password:", password);
+console.log("Email:", email);
+
+///////////////////////////////////////////////
+const counties = ["Nairobi", "Mombasa", "Kajiado", "Nyeri", "Nyandarua"];
+console.log(counties[1]);
